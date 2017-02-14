9faa12e74fa12f04b01699f29478cd4c6bb7422b
Swen <swenmeeu@gmail.com>
2017-02-14 10:59:59 +0100

    Added: Vector implementation

---
+++ Assignment1/K-means clustering/K-means clustering/Vector.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K_means_clustering
+{
+    class Vector : IEnumerable
+    {
+        float[] dimensions;
+
+        public double length
+        {
+            get
+            {
+                double squaredDimensions = 0;
+                foreach (var dimension in dimensions)
+                {
+                    squaredDimensions += Math.Pow(dimension, 2);
+                }
+                return Math.Sqrt(squaredDimensions);
+            }
+        }
+
+        public Vector()
+        {
+
+        }
+
+        public Vector(params float[] dimensions)
+        {
+            this.dimensions = dimensions;
+        }
+
+        public double Distance(Vector v2)
+        {
+            if (this.dimensions.Length != v2.dimensions.Length)
+                throw new Exception("Can't compute the distance between vectors with a different amount of dimensions.");
+
+            double squaredDimensions = 0;
+            for (int i = 0; i < dimensions.Length; i++)
+            {
+                squaredDimensions += Math.Pow(this.dimensions[i] - v2.dimensions[i], 2);
+            }
+            return Math.Sqrt(squaredDimensions);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return dimensions.GetEnumerator();
+        }
+
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            if (v1.dimensions.Length != v2.dimensions.Length)
+                throw new Exception("Can't compute the distance between vectors with a different amount of dimensions.");
+
+            float[] dimensions = new float[v1.dimensions.Length];
+            for (int i = 0; i < v1.dimensions.Length; i++)
+            {
+                dimensions[i] += v1.dimensions[i] + v2.dimensions[i];
+            }
+            return new Vector(dimensions);
+        }
+
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            if (v1.dimensions.Length != v2.dimensions.Length)
+                throw new Exception("Can't compute the distance between vectors with a different amount of dimensions.");
+
+            float[] dimensions = new float[v1.dimensions.Length];
+            for (int i = 0; i < v1.dimensions.Length; i++)
+            {
+                dimensions[i] += v1.dimensions[i] - v2.dimensions[i];
+            }
+            return new Vector(dimensions);
+        }
+    }
+}
