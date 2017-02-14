5053c11ba144473b83e3719e1d6e26db3508a5e2
Swen <swenmeeu@gmail.com>
2017-02-14 12:47:30 +0100

    Added: Cluster model

---
+++ Assignment1/K-means clustering/K-means clustering/Cluster.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K_means_clustering
+{
+    class Cluster
+    {
+        public int id { get; set; }
+        public Vector centroid { get; set; }
+        public Vector[] observations { get; set; }
+
+        public Cluster()
+        {
+
+        }
+
+        public Cluster(int id, Vector centroid, Vector[] observation)
+        {
+            this.id = id;
+            this.centroid = centroid;
+            this.observations = observations;
+        }
+    }
+}
