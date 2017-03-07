0e8b1e04dae143bbd49b84b438f0c25979e6dbc7
Swen <swenmeeu@gmail.com>
2017-03-07 15:28:09 +0100

    Defined selection methods

---
+++ Assignment2/Genetic algorithm assignment/Genetic algorithm assignment/SectionMethod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genetic_algorithm_assignment
+{
+    enum SectionMethod
+    {
+        ROULETTE,
+        RANK,
+        TOURNAMENT
+    }
+}
