8a2b16318606b0198438e1a0e947772832ce48f6
Swen <swenmeeu@gmail.com>
2017-03-07 15:07:39 +0100

    Defined a population

---
+++ Assignment2/Genetic algorithm assignment/Genetic algorithm assignment/Population.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genetic_algorithm_assignment
+{
+    class Population
+    {
+        Individual[] individuals;
+        public Population(Individual[] individuals)
+        {
+            this.individuals = individuals;
+        }
+
+        public static Population CreateRandomPopulation(int size)
+        {
+            Individual[] individuals = new Individual[size];
+            for (int i = 0; i < individuals.Length; i++)
+            {
+                individuals[i] = Individual.CreateRandomIndividual();
+            }
+            return new Population(individuals);
+        }
+
+        public Population Evolve(bool useElitism)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assignment2/Genetic algorithm assignment/Genetic algorithm assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genetic_algorithm_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Population population = Population.CreateRandomPopulation(200);
+
+            Individual ind1 = Individual.CreateRandomIndividual();
+            Individual ind2 = Individual.CreateRandomIndividual();
+            Console.WriteLine(ind1);
+            Console.WriteLine(ind2);
+
+            Console.WriteLine(ind1.Fitness);
+            Console.WriteLine(ind2.Fitness);
+
+            Console.ReadLine();
+        }
+    }
+}
