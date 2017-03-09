4b38acdaeff46cd785217e26903838789ca3a2d0
swenmeeuwes <swenmeeu@gmail.com>
2017-03-10 00:42:45 +0100

    Added execution of the 'Generic genetic algorithm' with 'roulette' selection

--- Assignment2/Genetic algorithm assignment/Genetic algorithm assignment/Program.cs
+++ Assignment2/Genetic algorithm assignment/Genetic algorithm assignment/Program.cs
@@ -10,15 +10,19 @@ namespace Genetic_algorithm_assignment
     {
         static void Main(string[] args)
         {
-            Population population = Population.CreateRandomPopulation(200);
+            var population = Population.CreateRandomPopulation(10);
+            var evolvedPopulation = population.GenericAlgorithm(0.95f, 0.01f, 10);
 
-            Individual ind1 = Individual.CreateRandomIndividual();
-            Individual ind2 = Individual.CreateRandomIndividual();
-            Console.WriteLine(ind1);
-            Console.WriteLine(ind2);
+            Console.WriteLine("INFORMATION ABOUT THE LAST GENERATION");
 
-            Console.WriteLine(ind1.Fitness);
-            Console.WriteLine(ind2.Fitness);
+            var averageFitness = evolvedPopulation.individuals.Average((individual) => (individual.Fitness));
+            Console.WriteLine("Average fitness: {0}", averageFitness);
+
+            var bestFitness = evolvedPopulation.individuals.Max((individual) => (individual.Fitness));
+            Console.WriteLine("Best fitness: {0}", bestFitness);
+
+            var fittestIndividual = evolvedPopulation.individuals.Select((individual) => (individual)).Where((individual) => (individual.Fitness == bestFitness)).First();
+            Console.WriteLine("Best individual: {0}, decodation of genes: {1}", fittestIndividual, fittestIndividual.DecodeGenes());
 
             Console.ReadLine();
         }
