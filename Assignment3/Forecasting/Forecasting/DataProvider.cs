452ab60cf4d7eb8259873cf84d1464252a5fd7aa
swenmeeuwes <swenmeeu@gmail.com>
2017-03-17 22:34:58 +0100

    Added: Data provider

--- Assignment3/Forecasting/Forecasting/DataProvider.cs
+++ Assignment3/Forecasting/Forecasting/DataProvider.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,18 @@ namespace Forecasting
     {
         public Tuple<float, float>[] GetData()
         {
-
+            List<Tuple<float, float>> points = new List<Tuple<float, float>>();
+            using (StreamReader streamReader = new StreamReader("SwordForecasting.csv"))
+            {
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    var dimensions = line.Split(';');
+                    var point = new Tuple<float, float>(float.Parse(dimensions[0]), float.Parse(dimensions[1]));
+                    points.Add(point);
+                }
+            };
+            return points.ToArray();
         }
     }
 }
